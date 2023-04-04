8479e47359fc3473e35f01982c8a34a1a443a088
Roman Babik <kazantip2020rb@gmail.com>
2023-04-04 15:09:35 +0300

    save task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const refs = {
+    btnStart: document.querySelector("[data-start]"),
+    btnStop: document.querySelector("[data-stop]"),
+    bodyDoc: document.querySelector("body")
+}
+
+let idInterval = null; 
+
+refs.btnStart.addEventListener("click", () => {
+    idInterval = setInterval(function startGenerateBackground() {
+    const randomColor = getRandomHexColor();
+    refs.bodyDoc.style.backgroundColor = randomColor;
+    refs.btnStart.disabled = true;
+}, 1000);});
+
+refs.btnStop.addEventListener("click", () => {
+    clearInterval(idInterval);
+    refs.btnStart.disabled = false;
+});
+
+
 
